3d74fe8e51a282d8808c657f88ef7d9dfb8a9893
Mael Lacour <public.dev@maellacour.com>
2023-05-03 10:22:58 +0200

    Add Gradient Constant atom variable

---
+++ Runtime/AtomsHelpers/GradientConstant.cs
@@ -0,0 +1,11 @@
+namespace EyapLibrary.AtomsHelpers
+{
+	using UnityEngine;
+	using UnityAtoms;
+
+	/// <summary>
+	/// Constant of type `Gradient`. Inherits from `AtomBaseVariable<Gradient>`.
+	/// </summary>[EditorIcon("atom-icon-teal")]
+	[CreateAssetMenu(menuName = "Unity Atoms/Constants/Gradient", fileName = "GradientConstant")]
+	public sealed class GradientConstant : AtomBaseVariable<Gradient> { }
+}
